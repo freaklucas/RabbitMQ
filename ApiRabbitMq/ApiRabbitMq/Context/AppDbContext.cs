1d1cc868a15f1f5b3d92f7e1dde32fcc09cad58c
freaklucas <lucas.oliveira.freak@gmail.com>
2024-09-02 23:09:23 -0300

    feat:init

---
+++ ApiRabbitMq/ApiRabbitMq/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiRabbitMq.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRabbitMq.Context;
+
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
