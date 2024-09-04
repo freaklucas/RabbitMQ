f7a76ec203a42a72d95f16c164102117febaa82c
freaklucas <lucas.oliveira.freak@gmail.com>
2024-09-03 23:36:40 -0300

    feat:registrando serviço

--- ApiRabbitMq/ApiRabbitMq/Program.cs
+++ ApiRabbitMq/ApiRabbitMq/Program.cs
@@ -1,3 +1,9 @@
+using ApiRabbitMq.Context;
+using ApiRabbitMq.RabbitMQ.Impl;
+using ApiRabbitMq.RabbitMQ.Interface;
+using ApiRabbitMq.Services;
+using ApiRabbitMq.Services.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IRabbitMQProducer, RabbitMQProducer>();
 
 var app = builder.Build();
 
