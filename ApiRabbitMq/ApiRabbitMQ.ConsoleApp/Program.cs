15a7ebb392407c2cfb55b99c4bef055416644ff9
freaklucas <lucas.oliveira.freak@gmail.com>
2024-09-05 23:18:04 -0300

    feat:definindo receptor

---
+++ ApiRabbitMq/ApiRabbitMQ.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Linq.Expressions;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("product", exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Mensagem do produto recebida: {message}");
+};
+
+channel.BasicConsume(queue: "product", autoAck: true, consumer: consumer);
+
+Console.ReadKey();
